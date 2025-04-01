ee9753f225bd1b71940adec026e6aa92e58913b1
takaaki5564 <rockwell5564@gmail.com>
2025-04-02 08:05:06 +0900

    Modified YOLOX train script

--- train_nano.sh
+++
@@ -1,1 +0,0 @@
-python3.11 tools/train.py -f yolox_nano_roof.py -d 1 -b 32 --fp16 -o -c ./model/yolox_nano.pth
--- yolox_nano_sideview.py
+++ yolox_nano_sideview.py
@@ -23,11 +23,11 @@ class Exp(MyExp):
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
 
         # Define yourself dataset path
-        self.data_dir = "datasets/coco20250326"  # ここを変更
+        self.data_dir = "dataset/coco_sideview"
         self.train_ann = "instances_train2017.json"
         self.val_ann = "instances_val2017.json"
 
-        self.num_classes = 11  # ここを変更
+        self.num_classes = 2  # ここを変更
         self.max_epoch = 200
 
     def get_model(self, sublinear=False):
